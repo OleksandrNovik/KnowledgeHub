bf27364edf8509f36023a75be0c820ef42b59636
Novik Oleksandr <novikoleksandr9@gmail.com>
2025-10-13 00:24:56 +0300

    Restored accidentally removed AddOpenApi method

--- KnowledgeHub.Api/Program.cs
+++ KnowledgeHub.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"))
